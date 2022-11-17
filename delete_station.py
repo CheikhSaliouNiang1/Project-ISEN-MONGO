39959aa0ef3180909cb4b2c5f3fac7c3b232eb52
Cheikh Saliou Niang <102148443+CheikhSaliouNiang1@users.noreply.github.com>
2022-11-17 22:18:35 +0100

    Update delete_station.py

--- delete_station.py
+++ delete_station.py
@@ -6,12 +6,12 @@ from pymongo.mongo_client import MongoClient
 
 client = MongoClient("mongodb+srv://Saliou:imwcVjBAhIZvwV9n@cluster0.e2aqyh7.mongodb.net/test", server_api=ServerApi('1'))
 db = client.vls
-def supprime_stations (_id_station):
+def supprime_stations (idStation):
     """
     supprimer une station par son id dans les deux collections
     """
 
-    db.stations.delete_one( {"_id":_id_station} )
+    db.stations.delete_one( {"_id":idStation} )
 
-    db.datas.delete_many( {"station_id":_id_station} )
+    db.datas.delete_many( {"station_id":idStation} )
 supprime_stations(41)
